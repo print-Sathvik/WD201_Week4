5a565abde7238eb081a1d84dfbf1674c15b36e3e
unknown <satvikreddy2017@gmail.com>
2022-11-16 04:09:33 +0530

    All 5 tests passed and no errors from eslint

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+let today = new Date(new Date().toLocaleDateString("en-CA"));
+//Considering only date part of entire date
 
 describe("TodoList Test Suite", () => {
   beforeAll(() => {
@@ -27,5 +29,46 @@ describe("TodoList Test Suite", () => {
     expect(all[last_index].completed).toBe(false);
     markAsComplete(last_index); //marking last element as Complete
     expect(all[last_index].completed).toBe(true);
+  });
+
+  test("Checking Overdue items retrieval test case", () => {
+    let list = overdue();
+    let flag = true;
+    //Here even if one date is wrongly retrieved, the flag is set as false
+    for (let i = 0; i < list.length; i++) {
+      let due = new Date(list[i].dueDate);
+      if (today.getDate() <= due.getDate()) {
+        flag = false;
+      }
+    }
+    expect(flag).toBe(true);
+  });
+
+  test("Checking Due Today items retrieval test case", () => {
+    let list = dueToday();
+    let flag = true;
+    //Here even if one retrieved date is not same as today, the flag is set as false and test fails
+    for (let i = 0; i < list.length; i++) {
+      let due = new Date(list[i].dueDate);
+      if (today.getDate() == due.getDate()) {
+        //Do Nothing
+      } else {
+        flag = false;
+      }
+    }
+    expect(flag).toBe(true);
+  });
+
+  test("Checking Due Later items retrieval test case", () => {
+    let list = dueLater();
+    let flag = true;
+    //Here even if one date is wrongly retrieved, the flag is set as false
+    for (let i = 0; i < list.length; i++) {
+      let due = new Date(list[i].dueDate);
+      if (today.getDate() >= due.getDate()) {
+        flag = false;
+      }
+    }
+    expect(flag).toBe(true);
   });
 });
